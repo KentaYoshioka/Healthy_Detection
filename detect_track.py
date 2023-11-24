a29f85baaaaab02c069fb186bb5a35e66ec5a2df
yoshioka <ky19991008@gmail.com>
2023-11-24 16:32:48 +0900

    Update datect ver2

--- detect_track.py
+++ detect_track.py
@@ -1,65 +1,113 @@
 import cv2
 from ultralytics import YOLO
 import numpy as np
+import argparse
+import csv
+import datetime
+from argparse import RawTextHelpFormatter
+from evaluation import evaluation
 
-# 学習済みのモデルをロード
-model = YOLO('yolov8n.pt')
+COLOR = (0,255,0)
+VAR_THICKNESS = 20
+FONT_SCALE = 2.0
+TEXT_THICKNESS = 2
+FINISH_COMMAND = "q"
 
-# 動画ファイル(or カメラ)を開く
-cap = cv2.VideoCapture(0)
-#cap.set(cv2.CAP_PROP_FPS, 10)
 
-size = 100000 # 配列のサイズを設定
-pre_x1, pre_x2,  pre_y1,  pre_y2 = np.zeros(size, dtype=int), np.zeros(size, dtype=int), np.zeros(size, dtype=int), np.zeros(size, dtype=int)
-dis_x1, dis_x2,  dis_y1,  dis_y2 = np.zeros(size, dtype=int), np.zeros(size, dtype=int), np.zeros(size, dtype=int), np.zeros(size, dtype=int)
-pre_dis_x1, pre_dis_x2, pre_dis_y1, pre_dis_y2 = np.zeros(size, dtype=int), np.zeros(size, dtype=int), np.zeros(size, dtype=int), np.zeros(size, dtype=int)
+# set now_time
+dt_now = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
+# set deta_type
+tp = lambda x:list(map(int, x.split(',')))
 
-# キーが押されるまでループ
-while cap.isOpened():
-    success, frame = cap.read()
+# Format argment
+parser = argparse.ArgumentParser(formatter_class=RawTextHelpFormatter)
+parser.add_argument('--type', help='Set demo_type', default=1)
+parser.add_argument('--limit', help='Set numuber of detected people', default=100000)
+parser.add_argument('--fps', help='Set FPS', default=20)
+parser.add_argument('--outlier', help='Set validation', default=500)
+parser.add_argument('--model', help='Set model_data', default='yolov8n.pt')
+parser.add_argument('--framesize', type=tp, help='Set width and heigh of framesize', default='1920,1080')
+parser.add_argument('--input', help='Input video data', default=f'input_data/{dt_now}.mp4')
+parser.add_argument('--output', help='Output video data', default=f'output_data/{dt_now}.mp4')
+parser.add_argument('--csv', help='Output csv data', default=f'output_data/{dt_now}.csv')
 
-    if success:
-        # YOLOv8でトラッキング
-        results = model.track(frame, persist=True, classes=[0], tracker="bytetrack.yaml")
 
-        # キャプチャした画像サイズを取得
-        imageWidth = results[0].orig_shape[0]
-        imageHeight = results[0].orig_shape[1]
-        names = results[0].names
-        classes = results[0].boxes.cls
-        boxes = results[0].boxes
-        annotatedFrame = results[0].plot()
+parser.usage = parser.format_help()
 
-        # 結果を画像に変換
-        annotated_frame = results[0].plot()
+# Set argument
+args = parser.parse_args()
+fps = args.fps
+frame_width = args.framesize[0]
+frame_height = args.framesize[1]
+detect_limit = args.limit
+detect_type = args.type
+input_file = args.input
+output_file = args.output
+csv_file = args.csv
+model_data = args.model
+validation = args.outlier
 
-        for box, cls in zip(boxes, classes):
-            name = names[int(cls)]
-            x1, y1, x2, y2 = [int(i) for i in box.xyxy[0]]
-            if box.id is not None:
-                ids = int(box.id[0])
-            else:             continue  # または適切なデフォルト値を代入
+# Load learning model
+model = YOLO(model_data)
 
-            dis_x1[ids], dis_x2[ids], dis_y1[ids], dis_y2[ids] = abs(x1-pre_x1[ids]), abs(x2-pre_x2[ids]), abs(y1-pre_y1[ids]), abs(y2-pre_y2[ids])
-            evaluation = abs(dis_x1[ids]-pre_dis_x1[ids]) + abs(dis_x2[ids]-pre_dis_x2[ids]) + abs(dis_y1[ids]-pre_dis_y1[ids]) + abs(dis_y2[ids]-pre_dis_y2[ids])
-            pre_x1[ids], pre_x2[ids], pre_y1[ids], pre_y2[ids] = x1, x2, y1, y2
-            pre_dis_x1[ids], pre_dis_x2[ids], pre_dis_y1[ids], pre_dis_y2[ids] = dis_x1[ids], dis_x2[ids], dis_y1[ids], dis_y2[ids]
-            print(f"Object: {name} Coordinates: StartX={x1}, StartY={y1}, EndX={x2}, EndY={y2}")
+if detect_type == 1:
+    # Open camera
+    cap = cv2.VideoCapture(0)
+    cap.set(cv2.CAP_PROP_FPS, fps)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, frame_width)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, frame_height)
 
-            # バウンディングBOXの座標情報を書き込む
-            if evaluation < 500:
-                cv2.line(annotatedFrame, pt1=((x1, y2)),pt2=(x1 ,y2 - evaluation*4),color=(0,255,0),thickness=20,lineType=cv2.LINE_4, shift=0)
-            cv2.putText(annotatedFrame, f"EVALUATION {evaluation}", (x1, y1 - 40), cv2.FONT_HERSHEY_PLAIN, 2.0, (0, 255, 0), 2, cv2.LINE_AA)
+    # Set detected_data array
+    coordinate = np.array(np.zeros((4, detect_limit), dtype=int)).T.tolist()
+    velocity = np.array(np.zeros((4, detect_limit), dtype=int)).T.tolist()
+    pre_velocity = np.array(np.zeros((4, detect_limit), dtype=int)).T.tolist()
 
-        # プレビューウィンドウに画像出力
-        cv2.imshow("YOLOv8 Inference", annotatedFrame)
+    # loop of detection
+    while cap.isOpened():
+        success, frame = cap.read()
 
-        # アプリケーション終了
-        if cv2.waitKey(1) & 0xFF == ord("q"):
-            break
+        if success:
 
-# 終了処理
-cap.release()
-cv2.destroyAllWindows()
+            results = model.track(frame, persist=True, classes=[0], tracker="bytetrack.yaml")
+
+            # Get captured image
+            imageWidth = results[0].orig_shape[0]
+            imageHeight = results[0].orig_shape[1]
+            names = results[0].names
+            classes = results[0].boxes.cls
+            boxes = results[0].boxes
+            annotatedFrame = results[0].plot()
+
+            for box, cls in zip(boxes, classes):
+                x1, x2, y1, y2 = [int(i) for i in box.xyxy[0]]
+                name = names[int(cls)]
+                if box.id is not None:
+                    ids = int(box.id[0])
+                else:
+                    continue
+
+                velocity[ids][0], velocity[ids][1], velocity[ids][2], velocity[ids][3] = abs(x1-coordinate[ids][0]), abs(x2-coordinate[ids][1]), abs(y1-coordinate[ids][2]), abs(y2-coordinate[ids][3])
+                evaluation = int(abs(velocity[ids][0] - pre_velocity[ids][0]) + abs(velocity[ids][1] - pre_velocity[ids][1]) + abs(velocity[ids][2] - pre_velocity[ids][2]) + abs(velocity[ids][3] - pre_velocity[ids][3]))
+
+                # Update cordinate and velocity
+                coordinate[ids] = box.xyxy[0]
+                print(f"Object: {name} Coordinates: StartX={x1}, StartY={y1}, EndX={x2}, EndY={y2}")
+
+                LINE_START = (x1, y2)
+                LINE_FINISH = (x1, y2-evaluation*4)
+                ACTIVITY = (x1+15, y2-10)
+
+                # Write image
+                if evaluation < validation:
+                    cv2.line(annotatedFrame, pt1=LINE_START, pt2=LINE_FINISH, color=COLOR, thickness=VAR_THICKNESS, lineType=cv2.LINE_4)
+                cv2.putText(annotatedFrame, f"HUMAN ACTIVITY {evaluation}", ACTIVITY, cv2.FONT_HERSHEY_PLAIN, FONT_SCALE, COLOR, TEXT_THICKNESS, cv2.LINE_AA)
+
+            cv2.imshow("YOLOv8 Inference", annotatedFrame)
+
+            if cv2.waitKey(1) & 0xFF == ord(FINISH_COMMAND):
+                break
+
+    cap.release()
+    cv2.destroyAllWindows()
 
 
